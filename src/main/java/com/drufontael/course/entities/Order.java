6e5273de29708f7f212cf439c3440a3debf9c607
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-08-30 10:34:12 -0300

    OrderStatus enum

--- src/main/java/com/drufontael/course/entities/Order.java
+++ src/main/java/com/drufontael/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.drufontael.course.entities;
 
+import com.drufontael.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -16,6 +17,7 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -24,9 +26,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -44,6 +47,14 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus!=null) this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
