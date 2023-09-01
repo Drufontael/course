6f5a58ed7fa4653158b494a32bb43ee533751065
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-09-01 12:20:09 -0300

    Exception handling - update

--- src/main/java/com/drufontael/course/services/UserService.java
+++ src/main/java/com/drufontael/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.drufontael.course.entities.User;
 import com.drufontael.course.repositories.UserRepository;
 import com.drufontael.course.services.exceptions.DatabaseException;
 import com.drufontael.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -37,9 +38,15 @@ public class UserService {
 
     }
     public User update(Long id,User obj){
-        User entity=repository.getReferenceById(id);
-        updateData(entity,obj);
-        return repository.save(entity);
+
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity,obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
